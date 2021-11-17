8341ad62dd2051e2367e601a48e9198542c87a62
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-17 12:19:32 +0200

    .

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,101 @@
-@extends('layouts.app')
+@section('page_name'){{ 'Forgot Password' }}@endsection
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@include('inc.header')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<!-- =====================================
+	    	==== Start breadcrumb -->
+<div class="breadcrumb section-bg" style="background-image:url(/assets/images/bg_breadcrumb.jpg)">
+    <!-- container -->
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-12">
+                <h1>@yield('page_name')</h1>
+                <ol class="item-breadcrumb">
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li><span aria-hidden="true">›</span></li>
+                    <li>@yield('page_name')</li>
+                </ol>
             </div>
         </div>
     </div>
+    <!-- /container -->
 </div>
-@endsection
+<!-- =====================================
+             ==== End breadcrumb -->
+
+<!-- =====================================
+             ==== Start account -->
+<div class="main-content">
+    <div class="page-account">
+        <!-- container -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                    <div class="account-wrapper">
+                        <div class="tab-content">
+                            <div class="account-form-container login-form">
+                                <div class="account-form">
+                                    <form action="{{ route('password.update') }}" method="post">
+                                        @csrf
+
+                                        <input type="hidden" name="token" value="{{ $token }}">
+
+                                        <div class="mb-3">
+                                            <label for="email">Email</label>
+                                            <input type="email" name="email" id="email"
+                                                class="form-control @error('email') is-invalid @enderror"
+                                                placeholder="Email" value="{{ $email ?? old('email') }}" required>
+
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="password">Password</label>
+                                            <input type="password" name="password" id="password"
+                                                class="form-control @error('email') is-invalid @enderror"
+                                                placeholder="Password" required>
+
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="password_confirmation">Password Confirmation</label>
+                                            <input type="password" name="password_confirmation" id="password_confirmation"
+                                                class="form-control @error('password_confirmation') is-invalid @enderror"
+                                                placeholder="Password" required>
+
+                                            @error('password_confirmation')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="button-box">
+                                            <button type="submit"
+                                                class="btn btn-default btn-normal"><span>Submit</span></button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+    </div>
+</div>
+<!-- =====================================
+             ==== End account -->
+
+
+@include('inc.footer')
