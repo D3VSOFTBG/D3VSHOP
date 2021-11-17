051a9b61a97d384465179a7ac44708540c7840e4
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-17 02:31:00 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Auth::routes();
