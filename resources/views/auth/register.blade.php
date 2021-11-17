183eb277e140ed063cf54bc5c2c0ec944940c14b
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-17 05:09:52 +0200

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,65 @@
-@extends('layouts.app')
+@section('page_name'){{ 'Register' }}@endsection
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+@include('inc.header')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<!-- =====================================
+	    	==== Start breadcrumb -->
+<div class="breadcrumb section-bg" style="background-image:url(assets/images/bg_breadcrumb.jpg)">
+    <!-- container -->
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-12">
+                <h1>@yield('page_name')</h1>
+                <ol class="item-breadcrumb">
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li><span aria-hidden="true">›</span></li>
+                    <li>@yield('page_name')</li>
+                </ol>
             </div>
         </div>
     </div>
+    <!-- /container -->
 </div>
-@endsection
+<!-- =====================================
+             ==== End breadcrumb -->
+
+<!-- =====================================
+             ==== Start account -->
+<div class="main-content">
+    <div class="page-account">
+        <!-- container -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                    <div class="account-wrapper">
+                        <div class="tab-content">
+                            <div class="account-form-container register-form">
+                                <div class="account-form">
+                                    <form action="{{route('login')}}" method="post">
+                                        <input type="text" name="first-name" class="form-control"
+                                            placeholder="First Name">
+                                        <input type="text" name="last-name" class="form-control"
+                                            placeholder="Last Name">
+                                        <input name="user-email" type="email" class="form-control"
+                                            placeholder="Email">
+                                        <input type="password" class="form-control" placeholder="Password">
+                                        <div class="button-box">
+                                            <button type="submit"
+                                                class="btn btn-default btn-normal"><span>Register</span></button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+    </div>
+</div>
+<!-- =====================================
+             ==== End account -->
+
+
+@include('inc.footer')
