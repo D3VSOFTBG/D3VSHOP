d46cdb8a6b0fe853048693eb9877be014173409a
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-18 10:04:41 +0200

    .

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -38,6 +38,12 @@
                                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                                         @csrf
 
+                                        @if (session('resent'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ __('A fresh verification link has been sent to your email address.') }}
+                                        </div>
+                                        @endif
+
                                         <p>
                                             <small>
                                                 {{ __('Before proceeding, please check your email for a verification link.') }}
