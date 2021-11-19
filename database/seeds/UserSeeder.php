d6b6b21fba4ec870e3f1559cc8b49f5f058e557e
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-19 10:46:11 +0200

    .

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin Account',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('4*9~!%KNqyW#N[!P'),
+                'role' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'User2 Account',
+                'email' => 'user2@gmail.com',
+                'password' => Hash::make('4*9~!%KNqyW#N[!P'),
+                'role' => NULL,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'User Account',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('4*9~!%KNqyW#N[!P'),
+                'role' => NULL,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
