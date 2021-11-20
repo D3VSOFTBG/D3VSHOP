fd37a2c20450f1b7712ebe0614fc5727fa6a28a2
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-20 18:11:09 +0200

    .

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>404</title>
+    <title>500</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
