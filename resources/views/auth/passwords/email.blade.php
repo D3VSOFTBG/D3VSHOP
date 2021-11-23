75bc275ad7f9038b488b5d1f6924e96051673666
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-23 21:23:13 +0200

    .

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,79 +1,51 @@
 @section('page_name'){{ 'Forgot Password' }}@endsection
 
-@include('inc.header')
+@include('auth.inc.header')
 
-<!-- =====================================
-	    	==== Start breadcrumb -->
-<div class="breadcrumb section-bg" style="background-image:url(/assets/images/bg_breadcrumb.jpg)">
-    <!-- container -->
-    <div class="container">
-        <div class="row">
-            <div class="col col-md-12">
-                <h1>@yield('page_name')</h1>
-                <ol class="item-breadcrumb">
-                    <li><a href="{{route('home')}}">Home</a></li>
-                    <li><span aria-hidden="true">›</span></li>
-                    <li>@yield('page_name')</li>
-                </ol>
-            </div>
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="{{route('home')}}" class="h1">{{Cache::get('shop_name')}}</a>
         </div>
-    </div>
-    <!-- /container -->
-</div>
-<!-- =====================================
-             ==== End breadcrumb -->
+        <div class="card-body">
+            <p class="login-box-msg">@yield('page_name')</p>
 
-<!-- =====================================
-             ==== Start account -->
-<div class="main-content">
-    <div class="page-account">
-        <!-- container -->
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
-                    <div class="account-wrapper">
-                        <div class="tab-content">
-                            <div class="account-form-container">
-                                <div class="account-form">
-                                    <form action="{{ route('password.email') }}" method="post">
-                                        @csrf
+            <form class="mb-3" action="{{route('password.email')}}" method="post">
+                @csrf
 
-                                        @if (session('status'))
-                                        <div class="alert alert-success" role="alert">
-                                            {{ session('status') }}
-                                        </div>
-                                        @endif
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+                @endif
 
-                                        <div class="mb-3">
-                                            <label for="email">Email</label>
-                                            <input type="email" name="email" id="email"
-                                                class="form-control @error('email') is-invalid @enderror"
-                                                placeholder="Email" required>
-
-                                            @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-
-                                        <div class="button-box">
-                                            <button type="submit"
-                                                class="btn btn-default btn-normal"><span>Submit</span></button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
+                <div class="input-group mb-3">
+                    <input name="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" required>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
                         </div>
                     </div>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
-            </div>
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Send</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
         </div>
-        <!-- /container -->
+        <!-- /.card-body -->
     </div>
+    <!-- /.card -->
 </div>
-<!-- =====================================
-             ==== End account -->
+<!-- /.login-box -->
 
-
-@include('inc.footer')
+@include('auth.inc.footer')
