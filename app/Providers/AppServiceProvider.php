f7133f0824d1d7d4d7d14f45e028093e80ca62be
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-29 21:01:58 +0200

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,13 @@
 
 namespace App\Providers;
 
-use App\Setting;
-use Illuminate\Support\ServiceProvider;
 // use Illuminate\Support\Facades\DB;
 // use Illuminate\Support\Facades\View;
 // use Illuminate\Contracts\Cache\Factory;
 // use Illuminate\Contracts\Cache\Repository;
+use App\Setting;
+use App\Currency;
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Cache;
 
 class AppServiceProvider extends ServiceProvider
@@ -32,9 +33,10 @@ class AppServiceProvider extends ServiceProvider
         if(!Cache::has('shop_name'))
         {
             $settings = Setting::all();
+            $default_currency = Currency::where('id', $settings[2]['value'])->pluck('code')->first();
             Cache::forever('shop_name', $settings[0]['value']);
             Cache::forever('title_seperator', $settings[1]['value']);
-            Cache::forever('default_currency', $settings[2]['value']);
+            Cache::forever('default_currency', $default_currency);
             Cache::forever('theme_name', $settings[3]['value']);
         }
     }
