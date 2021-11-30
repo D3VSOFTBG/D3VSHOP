7b4ccf1f92c706cfa7392ec1df4a3b7ee9fb81fa
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-11-30 14:30:16 +0200

    .

--- app/Jobs/StripeWebhooks/ChargeSucceeded.php
+++ app/Jobs/StripeWebhooks/ChargeSucceeded.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Spatie\WebhookClient\Models\WebhookCall;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class ChargeSucceeded implements ShouldQueue
 {
@@ -29,6 +30,23 @@ class ChargeSucceeded implements ShouldQueue
         Log::info($charge);
 
         // do your work here
+
+        DB::table('orders')->insert(
+            [
+                'currency_id' => 1,
+                'customer' => $charge['billing_details']['name'],
+                'total' => $charge['amount'] / 100,
+                'phone' => '0876595156',
+                'email' => $charge['billing_details']['email'],
+                'country' => $charge['billing_details']['address']['country'],
+                'city' => $charge['billing_details']['address']['city'],
+                'address_1' => $charge['billing_details']['address']['line1'],
+                'address_2' => $charge['billing_details']['address']['line2'],
+                'postal_code' => $charge['billing_details']['address']['postal_code'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
 
         // you can access the payload of the webhook call with `$this->webhookCall->payload`
     }
