84d9b630108558b661098981cce5b875db9a70db
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-12-03 21:26:12 +0200

    .

--- resources/views/admin/shop/orders.blade.php
+++ resources/views/admin/shop/orders.blade.php
@@ -172,12 +172,12 @@
                                                             <div class="modal-body">
                                                                 <nav>
                                                                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                                                      <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+                                                                      <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home{{$order->id}}" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
                                                                       <a class="nav-link" id="nav-products-tab" data-toggle="tab" href="#nav-products{{$order->id}}" role="tab" aria-controls="nav-products" aria-selected="false">Products (<small>{{$count}}</small>)</a>
                                                                     </div>
                                                                   </nav>
                                                                   <div class="tab-content" id="nav-tabContent">
-                                                                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                                                                    <div class="tab-pane fade show active" id="nav-home{{$order->id}}" role="tabpanel" aria-labelledby="nav-home-tab">
                                                                         <table class="table m-0">
                                                                             <tr>
                                                                                 <th>
