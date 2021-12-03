69ec8d5bc1697d15ebfc5d67ad7cf7a73d18ec2a
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-12-04 01:20:52 +0200

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,15 +25,4 @@ class HomeController extends Controller
     {
         return redirect(route('home'))->withErrors('Error!');
     }
-    function c()
-    {
-
-        //require_once dirname(__FILE__) . './vendor/autoload.php';
-
-        $options = array('facade' => 'DB::');
-        $converter = new SQLToLaravelBuilder($options);
-
-        $sql = "SELECT orders.*, (SELECT IF(coalesce(sum(ordered_products.price), '') = '', '0', sum(ordered_products.price)) FROM ordered_products ordered_products WHERE ordered_products.order_id = orders.id) as total FROM orders";
-        echo $converter->convert($sql);
-    }
 }
