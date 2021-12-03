9969f33674089eb3206d8cb2b42df01ea122b889
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-12-04 01:11:18 +0200

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,8 @@ use App\Product;
 use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\Log;
 //use Illuminate\Support\Facades\Cache;
+use RexShijaku\SQLToLaravelBuilder\SQLToLaravelBuilder;
+
 
 
 class HomeController extends Controller
@@ -22,5 +24,16 @@ class HomeController extends Controller
     function cancel()
     {
         return redirect(route('home'))->withErrors('Error!');
+    }
+    function c()
+    {
+
+        //require_once dirname(__FILE__) . './vendor/autoload.php';
+
+        $options = array('facade' => 'DB::');
+        $converter = new SQLToLaravelBuilder($options);
+
+        $sql = "SELECT orders.*, (SELECT IF(coalesce(sum(ordered_products.price), '') = '', '0', sum(ordered_products.price)) FROM ordered_products ordered_products WHERE ordered_products.order_id = orders.id) as total FROM orders";
+        echo $converter->convert($sql);
     }
 }
