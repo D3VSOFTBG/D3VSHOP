c6faf74f9e7b0c6adcfae64ebb36221109dfe2d7
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-12-04 03:09:18 +0200

    .

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Currency;
 use App\Order;
+use App\Ordered_Product;
 use Illuminate\Http\Request;
 use LaravelDaily\Invoices\Invoice;
 use LaravelDaily\Invoices\Classes\Buyer;
@@ -23,7 +24,16 @@ class InvoiceController extends Controller
             ],
         ]);
 
-        $item = (new InvoiceItem())->title($order->name)->pricePerUnit(1)->quantity(2);
+        $ordered_products = Ordered_Product::where('id', $id)->get();
+
+        // $items = array();
+
+        // foreach($ordered_products as $op)
+        // {
+        //     array_push($items, (new InvoiceItem())->title($op->name)->pricePerUnit(71.96)->quantity(2));
+        // }
+
+        (new InvoiceItem())->title($op->name)->pricePerUnit(71.96)->quantity(2);
 
         $invoice = Invoice::make("Invoice $id")
             //->series('BIG')
@@ -35,7 +45,7 @@ class InvoiceController extends Controller
             ->discountByPercent(0)
             ->taxRate(0)
             ->shipping(1)
-            ->addItem($item)
+            ->addItems($items)
             ->logo(public_path('vendor/invoices/sample-logo.png'))
             ->filename('invoice_' . $id);
 
