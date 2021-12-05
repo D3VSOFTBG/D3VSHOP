c6a7000f5480aca364113bb5557bcc72d0dd7601
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-12-05 23:42:54 +0200

    .

--- app/Http/Controllers/Payments/StripeController.php
+++ app/Http/Controllers/Payments/StripeController.php
@@ -92,6 +92,8 @@ class StripeController extends Controller
         $stripe_logs->invoice_id = $invoice['id'];
         $stripe_logs->save();
 
+        Log::info($customer['id']);
+
         return redirect($session->url)->withStatus(303);
     }
 }
