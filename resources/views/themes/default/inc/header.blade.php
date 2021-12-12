6b989c7064cb6879eb2ac3978a4d8b79088b8479
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2021-12-12 13:26:58 +0200

    .

--- resources/views/themes/default/inc/header.blade.php
+++ resources/views/themes/default/inc/header.blade.php
@@ -109,7 +109,12 @@
                         <i class="fa fa-fw fa-user text-dark mr-3 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></i>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
                             @if (Auth::check())
-                            <li><a class="dropdown-item" href="{{route('logout')}}">Logout</a></li>
+                            <li>
+                                <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
                             @else
                             <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
                             <li><a class="dropdown-item" href="{{route('register')}}">Register</a></li>
