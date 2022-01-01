6cea900443cbbfeda711241e7f8abba4fcb87566
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-01 21:11:30 +0200

    .

--- packages/D3VSOFT/Theme/src/resources/views/home.blade.php
+++ packages/D3VSOFT/Theme/src/resources/views/home.blade.php
@@ -16,7 +16,7 @@
             <div class="container">
                 <div class="row p-5">
                     <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                        <img class="img-fluid" src="{{asset('/themes/default/assets/img/banner_img_01.jpg')}}" alt="">
+                        <img class="img-fluid" src="{{asset("$theme_path/assets/img/banner_img_01.jpg")}}" alt="">
                     </div>
                     <div class="col-lg-6 mb-0 d-flex align-items-center">
                         <div class="text-align-left align-self-center">
@@ -52,7 +52,7 @@
             <div class="container">
                 <div class="row p-5">
                     <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                        <img class="img-fluid" src="{{asset('/themes/default/assets/img/banner_img_03.jpg')}}" alt="">
+                        <img class="img-fluid" src="{{asset("$theme_path/assets/img/banner_img_03.jpg")}}" alt="">
                     </div>
                     <div class="col-lg-6 mb-0 d-flex align-items-center">
                         <div class="text-align-left">
