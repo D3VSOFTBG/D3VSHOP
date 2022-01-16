53230fe77fe7a7a783dd669139909be7e4201675
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2022-01-16 19:04:54 +0200

    .

--- app/Setting.php
+++ app/Setting.php
@@ -4,8 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AdminMenu extends Model
+class Setting extends Model
 {
-    protected $table = 'admin_menus';
     public $timestamps = false;
 }
